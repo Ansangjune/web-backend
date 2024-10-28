837d772847257217bbfac588f0753b20c9912c93
“Ansangjun” <platinum4903@gmail.com>
2024-10-28 21:13:58 +0900

    Add lombok and refactoring the code. Add logging code.

--- src/main/java/com/example/webbackend/controller/ArticleController.java
+++ src/main/java/com/example/webbackend/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.webbackend.controller;
 import com.example.webbackend.dto.ArticleForm;
 import com.example.webbackend.entity.Article;
 import com.example.webbackend.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
     @Autowired
@@ -20,13 +22,13 @@ public class ArticleController {
 
     @PostMapping ("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
         // DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
         // 레파지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
--- src/main/java/com/example/webbackend/dto/ArticleForm.java
+++ src/main/java/com/example/webbackend/dto/ArticleForm.java
@@ -1,23 +1,14 @@
 package com.example.webbackend.dto;
 
 import com.example.webbackend.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
     public String title;
     public String content;
-
-    public ArticleForm(String content, String title) {
-        this.content = content;
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
