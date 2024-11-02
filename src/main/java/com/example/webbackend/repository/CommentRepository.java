29d551bd28fe13351be52d8e62e6102b3e50e174
“Ansangjun” <platinum4903@gmail.com>
2024-11-02 15:02:40 +0900

    Add test code for comment Repository.

--- src/main/java/com/example/webbackend/repository/CommentRepository.java
+++ src/main/java/com/example/webbackend/repository/CommentRepository.java
@@ -10,7 +10,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     // 특정 계시글의 모든 댓글 조회
     @Query(value = "SELECT * FROM comment WHERE article_id = :articleId",
             nativeQuery = true) // value 속성에 실행하려는 쿼리 작성
-    List<Comment> findArticleId(Long articleId);
+    List<Comment> findByArticleId(Long articleId);
     // 특정 닉네임의 모든 댓글 조회
     List<Comment> findByNickname(String nickname);
 }
